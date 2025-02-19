54450d4bf2ec00d6cc289857837f4d5c1b8c29d2
ST10248202 <st10248202@vcconnect.edu.za>
2025-02-19 09:41:54 +0200

    Created new view and added a 'Start' button

---
+++ app/src/main/java/com/fake/prog7313_addapp/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.fake.prog7313_addapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val buttonStart = findViewById<Button>(R.id.buttonStart)
+
+        buttonStart.setOnClickListener{
+
+            val navigate = Intent(this, MainActivity::class.java)
+            startActivity(navigate)
+        }
+
+    }
+}
